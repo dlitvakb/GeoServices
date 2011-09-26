46287b2a392bbf33144176418d1a313e7766fd7f
David Litvak <david.litvakb@gmail.com>
2011-09-26 10:19:06 -0300

    editingWorkspace

---
+++ GeoServices-Test/SDE/EditingWorkspaceTest.vb
@@ -0,0 +1,63 @@
+﻿Imports System.Text
+
+<TestClass()>
+Public Class EditingWorkspaceTest
+
+    Private testContextInstance As TestContext
+
+    '''<summary>
+    '''Gets or sets the test context which provides
+    '''information about and functionality for the current test run.
+    '''</summary>
+    Public Property TestContext() As TestContext
+        Get
+            Return testContextInstance
+        End Get
+        Set(ByVal value As TestContext)
+            testContextInstance = Value
+        End Set
+    End Property
+
+#Region "Additional test attributes"
+    '
+    ' You can use the following additional attributes as you write your tests:
+    '
+    ' Use ClassInitialize to run code before running the first test in the class
+    ' <ClassInitialize()> Public Shared Sub MyClassInitialize(ByVal testContext As TestContext)
+    ' End Sub
+    '
+    ' Use ClassCleanup to run code after all tests in a class have run
+    ' <ClassCleanup()> Public Shared Sub MyClassCleanup()
+    ' End Sub
+    '
+    ' Use TestInitialize to run code before running each test
+    ' <TestInitialize()> Public Sub MyTestInitialize()
+    ' End Sub
+    '
+    ' Use TestCleanup to run code after each test has run
+    ' <TestCleanup()> Public Sub MyTestCleanup()
+    ' End Sub
+    '
+#End Region
+
+    <ClassInitialize()> Public Shared Sub MyClassInitialize(ByVal testContext As TestContext)
+        GeoServices.License.LicenseInitializer.InitializeLicense()
+    End Sub
+
+    <TestMethod()>
+    Public Sub WorkspaceVersionado()
+        Dim editWksp As New GeoServices.SDE.EditingWorkspace(New GeoServices.SDE.FeatureClassesGateway().GetByName("FAC_ACU_TRAMOS"))
+        Assert.IsTrue(editWksp.isVersioned)
+    End Sub
+
+    <TestMethod()>
+    Public Sub WorkspaceNoVersionado()
+        Dim editWksp As New GeoServices.SDE.EditingWorkspace(New GeoServices.SDE.TableGateway().GetByName("TBINST_ENCABEZADO_GEOGRAFICO"))
+        Assert.IsFalse(editWksp.isVersioned)
+    End Sub
+
+    <ClassCleanup()> Public Shared Sub MyClassCleanup()
+        GeoServices.License.LicenseInitializer.ReleaseLicense()
+    End Sub
+
+End Class
---
+++ GeoServices/SDE/EditingWorkspace.vb
@@ -0,0 +1,60 @@
+﻿Imports ESRI.ArcGIS.Geodatabase
+
+Namespace SDE
+    Public Class EditingWorkspace
+        Protected Property Dataset As IDataset
+
+        Public Sub New(ByVal dataset As IDataset)
+            If dataset Is Nothing Then Throw New DataException("No se ha enviado ningun Dataset")
+
+            Me.Dataset = dataset
+        End Sub
+
+        ''' <summary>
+        ''' Inicia sesión de edición
+        ''' </summary>
+        ''' <param name="WithUndoRedo"></param>
+        ''' <remarks></remarks>
+        Public Sub StartEditing(ByVal WithUndoRedo As Boolean)
+            If Me.isVersioned() Then
+                Dim wksp As IWorkspaceEdit2 = Me.getWorkspace()
+
+                wksp.StartEditing(WithUndoRedo)
+                wksp.StartEditOperation()
+            End If
+        End Sub
+
+        ''' <summary>
+        ''' Finaliza la sesión de edición
+        ''' </summary>
+        ''' <param name="SaveEdits"></param>
+        ''' <remarks></remarks>
+        Public Sub StopEditing(ByVal SaveEdits As Boolean)
+            If Me.isVersioned() Then
+                Dim wksp As IWorkspaceEdit2 = Me.getWorkspace()
+
+                wksp.StopEditOperation()
+                wksp.StopEditing(SaveEdits)
+            End If
+        End Sub
+
+        ''' <summary>
+        ''' Retorna el Workspace de edición
+        ''' </summary>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Protected Function getWorkspace() As IWorkspaceEdit2
+            Return Me.Dataset.Workspace
+        End Function
+
+        ''' <summary>
+        ''' Informa si el dataset se encuentra bajo control de versiones
+        ''' </summary>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Public Function isVersioned() As Boolean
+            Return CType(Me.Dataset, IVersionedObject3).IsRegisteredAsVersioned
+        End Function
+    End Class
+End Namespace
+
