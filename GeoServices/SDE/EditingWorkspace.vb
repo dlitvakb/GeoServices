ed03c843688655316f57ccd79ec0215b0912c867
David Litvak <david.litvakb@gmail.com>
2011-10-03 11:05:08 -0300

    changes?

--- GeoServices/SDE/EditingWorkspace.vb
+++ GeoServices/SDE/EditingWorkspace.vb
@@ -5,8 +5,14 @@ Namespace SDE
         Implements IEditingObject
 
         Protected Property Workspace As IWorkspace
+        Protected ReadOnly Property EditWorkspace As IWorkspaceEdit2
+            Get
+                Return CType(Me.Workspace, IWorkspaceEdit2)
+            End Get
+        End Property
 
         Public Sub New(ByVal workspace As IWorkspace)
+            If Not TypeOf workspace Is IWorkspaceEdit2 Then Throw New DataException("El workspace no es válido para edición")
             Me.Workspace = workspace
         End Sub
 
@@ -16,19 +22,19 @@ Namespace SDE
         End Function
 
         Public Sub StartEditing(ByVal WithUndoRedo As Boolean) Implements SDE.IEditingObject.StartEditing
-            If Me.isVersioned() Then Me.Workspace.StartEditing(WithUndoRedo)
+            If Me.isVersioned() Then Me.EditWorkspace.StartEditing(WithUndoRedo)
         End Sub
 
         Public Sub StartEditOperation() Implements SDE.IEditingObject.StartEditOperation
-            If Me.isVersioned() Then Me.Workspace.StartEditOperation()
+            If Me.isVersioned() Then Me.EditWorkspace.StartEditOperation()
         End Sub
 
         Public Sub StopEditOperation() Implements SDE.IEditingObject.StopEditOperation
-            If Me.isVersioned() Then Me.Workspace.StopEditOperation()
+            If Me.isVersioned() Then Me.EditWorkspace.StopEditOperation()
         End Sub
 
         Public Sub StopEditing(ByVal SaveEdits As Boolean) Implements SDE.IEditingObject.StopEditing
-            If Me.isVersioned() Then Me.Workspace.StopEditing(SaveEdits)
+            If Me.isVersioned() Then Me.EditWorkspace.StopEditing(SaveEdits)
         End Sub
     End Class
 End Namespace
