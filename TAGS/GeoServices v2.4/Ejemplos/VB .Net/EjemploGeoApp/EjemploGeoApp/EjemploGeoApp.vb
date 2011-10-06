c22b64acd72e458fd6f17014556217b72d3ae933
David Litvak <david.litvakb@gmail.com>
2011-10-06 14:41:44 -0300

    release v2.4

---
+++ TAGS/GeoServices v2.4/Ejemplos/VB .Net/EjemploGeoApp/EjemploGeoApp/EjemploGeoApp.vb
@@ -0,0 +1,27 @@
+﻿Module EjemploGeoApp
+
+    Sub Main()
+        Try
+            '' Inicializo el Logger
+            GeoServices.Logger.Logger.initialize()
+
+            '' Puedo usar el Logger en cualquier lugar una vez inicializado
+            GeoServices.Logger.Logger.Info("Empiezo a listar Feature Classes")
+
+            '' Hago una llamada a un WorkingComponent
+            Dim worker As GeoServices.Worker.WorkingComponent = New EjemploWorkingComponent()
+            worker.Execute()
+
+            '' Puedo usar el Logger en cualquier lugar una vez inicializado
+            GeoServices.Logger.Logger.Info("Terminé =)")
+
+                Console.ReadKey()
+            '' Imito la estructura de captura de excepciones de la plantilla ServiceHandler
+        Catch ex As UnauthorizedAccessException
+            GeoServices.Logger.Logger.Warn(ex)
+        Catch ex As Exception
+            GeoServices.Logger.Logger.Error(ex)
+        End Try
+    End Sub
+
+End Module
